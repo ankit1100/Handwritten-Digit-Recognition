ae010bf4b87dc8a0573c0db180cf94c66e5fa1ff
kunaljani1100 <30576356+kunaljani1100@users.noreply.github.com>
2020-05-06 18:24:33 +0530

    Add files via upload

---
+++ digit_recognizer_knn.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 21:25:56 2020
+
+@author: Kunal Jani
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#REading the data and storing them into the x and y variables.
+
+train_dataset=pd.read_csv('train.csv')
+
+y=train_dataset.iloc[:,0].values
+x=train_dataset.iloc[:,1:].values
+
+#Performing principal component analysis on the dataset
+
+from sklearn.decomposition import PCA
+pca=PCA(n_components=155)
+datavar=pca.fit_transform(x)
+
+#Normalizing the principal components
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+normalized_datavar = sc.fit_transform(datavar)
+
+#Splitting the data obtained from PCA into training data and testing data
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(normalized_datavar, y, test_size = 1/3, random_state = 0)
+
+#Classifying the test number images using the KNN Classifier
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+classifier.fit(x_train,y_train)
+
+#Calculate the predicted values as per the parameters calculated by our training model.
+
+y_pred = classifier.predict(x_test)
+
+#Calculation of acuracy by comparing each predicted and actual value.
+
+num_correct=0
+for i in range(len(y_pred)):
+    if(y_pred[i]==y_test[i]):
+        num_correct=num_correct+1 
+
+print(num_correct/len(y_pred))       
---
+++ digit_recognizer_svm.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  5 00:15:52 2020
+
+@author: Kunal Jani
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+train_dataset=pd.read_csv('train.csv')
+
+y=train_dataset.iloc[:,0].values
+x=train_dataset.iloc[:,1:].values
+
+from sklearn.decomposition import PCA
+pca=PCA(n_components=155)
+datavar=pca.fit_transform(x)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+normalized_datavar = sc.fit_transform(datavar)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+normalized_datavar = sc.fit_transform(datavar)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(normalized_datavar, y, test_size = 1/3, random_state = 0)
+
+y_train_values=[]
+for i in range(10):
+    train_values=[]
+    for value in y_train:
+        if(value==i):
+            train_values.append(1)
+        else:
+            train_values.append(0)
+    y_train_values.append(train_values)
+y_test_values=[]
+for i in range(10):
+    test_values=[]
+    for value in y_test:
+        if(value==i):
+            test_values.append(1)
+        else:
+            test_values.append(0)
+    y_test_values.append(test_values)
+    
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear', random_state = 0)
+y_pred_values=[]
+for i in range(10): 
+    classifier.fit(x_train, y_train_values[i])
+    y_pred_values.append(classifier.predict(x_test))
+
+y_pred=[]
+for i in range(len(y_pred_values[0])):
+    numbers=[]
+    for j in range(10):
+        if(y_pred_values[j][i]==1):
+            numbers.append(j)
+    if(len(numbers)==0):
+        numbers.append(-1)
+    y_pred.append(numbers)
+    
+num_correct=0 
+for i in range(len(y_test)):
+    if(len(y_pred[i])==1 and y_pred[i][0]==y_test[i]):
+        num_correct=num_correct+1
+        
+print(num_correct/len(y_test))
